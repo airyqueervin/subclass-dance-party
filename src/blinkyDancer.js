3e1df531d0e69612d58ac156516750dff396cb7f
Hack Reactor Student <student@hackreactor.com>
2017-04-03 17:56:50 -0700

    successfully refatored blinky dancer

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,37 +1,39 @@
-/*
 
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
 
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
+// var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+//   var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
 
-  var oldStep = blinkyDancer.step;
+//   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+//   // so we must keep a copy of the old version of this function
 
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
+//   var oldStep = blinkyDancer.step;
 
-  return blinkyDancer;
-};
+//   blinkyDancer.step = function() {
+//     // call the old version of step at the beginning of any call to this new version of step
+//     oldStep();
+//     // toggle() is a jQuery method to show/hide the <span> tag.
+//     // See http://api.jquery.com/category/effects/ for this and
+//     // other effects you can use on a jQuery-wrapped html tag.
+//     blinkyDancer.$node.toggle();
+//   };
 
-*/
+//   return blinkyDancer;
+// };
+
+
+
 
 var MakeBlinkyDancer = function(top, left, timeBetweenSteps) {
   MakeDancer.call(this, top, left, timeBetweenSteps);
-  this.oldStep = this.step;
-
+  console.log(this.step);
 };
 
 MakeBlinkyDancer.prototype = Object.create(MakeDancer.prototype);
 MakeBlinkyDancer.prototype.constructor = MakeBlinkyDancer;
+MakeBlinkyDancer.prototype.oldStep = MakeDancer.prototype.step;
 
 MakeBlinkyDancer.prototype.step = function () {
-  this.oldStep();
+  this.oldStep.call(this);
   this.$node.toggle();
 };
+
